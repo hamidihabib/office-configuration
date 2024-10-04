218cbffcf404028254d6980843072b42720ed82a
hamidihabib <hamidihabib@outlook.com>
2024-10-04 04:12:16 +0100

    test2

--- app/components/Switch.tsx
+++ app/components/Switch.tsx
@@ -1,13 +1,12 @@
 "use client";
+import classNames from "classnames";
 
-type SwitchProps = React.JSX.IntrinsicAttributes &
-  React.ClassAttributes<HTMLDivElement> &
-  React.HTMLAttributes<HTMLDivElement> & {
-    checked?: boolean;
-    onClick?: () => void;
-    children?: React.ReactNode;
-    radius?: boolean;
-  };
+type SwitchProps = React.HTMLAttributes<HTMLDivElement> & {
+  checked?: boolean;
+  onClick?: () => void;
+  children?: React.ReactNode;
+  radius?: boolean;
+};
 
 export default function Switch({
   children,
@@ -16,27 +15,30 @@ export default function Switch({
   onClick,
   ...props
 }: SwitchProps) {
-  const classes = {
-    button: [
-      ["bg-blue-500 ring-transparent flex-row-reverse", "bg-white  ring-black"][
-        checked ? 0 : 1
-      ],
-      "rounded-full flex h-5 ring-1 ring-inset",
-      ["w-5 ml-5", "w-10"][radius ? 0 : 1],
-    ].join(" "),
-    circle: [
-      ["bg-white", "bg-black"][checked ? 0 : 1],
-      "rounded-full w-3 h-3 m-1",
-    ].join(" "),
-  };
+  // Classes for button and circle
+  const buttonClasses = classNames(
+    "rounded-full flex h-5 ring-1 ring-inset transition-all duration-200",
+    checked ? "bg-blue-500 ring-transparent" : "bg-white ring-black",
+    radius ? "w-5 ml-5" : "w-10"
+  );
+
+  const circleClasses = classNames(
+    "rounded-full w-3 h-3 m-1 transition-all duration-200",
+    checked ? "bg-white" : "bg-black"
+  );
+
   return (
     <div
-      className={`flex items-center w-min ${children == "KMS" && "pb-5"}`}
+      className={`flex items-center w-min ${children === "KMS" ? "mb-5" : ""}`}
       {...props}
     >
       <label className="pr-2 w-28">{children}</label>
-      <button onClick={onClick} className={classes.button}>
-        <span className={classes.circle} />
+      <button
+        onClick={onClick}
+        className={buttonClasses}
+        aria-pressed={checked} // Improved accessibility
+      >
+        <span className={circleClasses} />
       </button>
       <label className="pl-2">{checked ? "On" : "Off"}</label>
     </div>
